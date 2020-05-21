066eb511a454a68b9a09f9e6179238980f9ebe5f
kingatan7 <kinga-tanska@wp.pl>
2020-05-21 16:49:34 +0200

    Checking user login and password. Start new activity
    Signed-off-by: kingatan7 <kinga-tanska@wp.pl>

--- app/src/main/java/com/example/child_health_record_system/MainActivity.java
+++ app/src/main/java/com/example/child_health_record_system/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.child_health_record_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +58,66 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    private class SendJSONtoServer extends AsyncTask<String, Void, String> {
+
+        private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        protected void onPreExecute() {
+            dialog.setMessage("Logowanie..");
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String data = "";
+
+            HttpURLConnection httpURLConnection = null;
+            try {
+
+                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+                wr.writeBytes("PostData=" + params[1]);
+                wr.flush();
+                wr.close();
+
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+                int inputStreamData = inputStreamReader.read();
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    data += current;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+            super.onPostExecute(result);
+            Log.e("TAG", result);
+            if (result.contains("accepted")) {// this is expecting a response code to be sent from your server upon receiving the POST data
+                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Nieprawidłowa nazwa użytkownika lub hasło",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
